dc0a31cb2a20517353827d12051d58abae400655
michael-leese <43089779+michael-leese@users.noreply.github.com>
2019-05-09 19:36:08 +0100

    test html file and testing push to github

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// Page Constants
+
+const Game_Width = 640;
+const Game_Height = 360;
+
+// Keep the game playing 
+
+const GameLive = true;
+
+
+
+// Select the Canvas and Context of the Canvas
+// #myGame and Context will be 2d
+
+var canvas = document.getElementById('myGame');
+var ctx = canvas.getContext('2d');
+
+
+
+
